23531ca17f5559fa3c316354c7b554bdf45e25d4
Alban Caré <alban.care@gmail.com>
2023-03-14 20:01:35 +0100

    feat: dispatch get articles action

--- src/components/App.js
+++ src/components/App.js
@@ -5,18 +5,23 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 
 import { getAllCategories } from '../actions/commonActions';
+import { getArticles } from '../actions/articlesActions';
 
 import config from '../config';
 
 function App() {
   const dispatch = useDispatch();
   const categories = useSelector((state) => state.common.categories);
+  const articles = useSelector((state) => state.articles.list);
 
   useEffect(() => {
     if (categories.length === 0) {
       dispatch(getAllCategories());
     }
-  }, [categories]);
+    if (articles.length === 0) {
+      dispatch(getArticles(18));
+    }
+  }, [categories, articles]);
 
   return (
     <Routes>
