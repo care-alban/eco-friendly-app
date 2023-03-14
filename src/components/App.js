59ffb82c72097b281eea6a45d2aee6295e08705e
Alban Caré <alban.care@gmail.com>
2023-03-14 17:02:38 +0100

    feat: dispatch get categories action

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,32 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
-import config from '../config';
 
+import Layout from './Layout';
 import HomePage from '../pages/HomePage';
 
+import { getAllCategories } from '../actions/commonActions';
+
+import config from '../config';
+
 function App() {
+  const dispatch = useDispatch();
+  const categories = useSelector((state) => state.common.categories);
+
+  useEffect(() => {
+    if (categories.length === 0) {
+      dispatch(getAllCategories());
+    }
+  }, [categories]);
+
   return (
-    <Routes>
-      <Route>
-        <Route path={`${config.basePath}`} element={<HomePage />} />
-      </Route>
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route>
+          <Route path={`${config.basePath}`} element={<HomePage />} />
+        </Route>
+      </Routes>
+    </Layout>
   );
 }
 
