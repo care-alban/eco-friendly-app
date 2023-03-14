a96b5336e3fcf000ceb2ea61181eb289d07704bd
Alban Caré <alban.care@gmail.com>
2023-03-14 12:45:53 +0100

    fix: you've called styled inside another component

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,27 +2,31 @@ import PropTypes from 'prop-types';
 
 import styled from 'styled-components';
 
-export default function Section({ id, sx, children }) {
-  const SectionSlyte = styled.section`
-    margin-bottom: 2rem;
-    ${sx}
-
-    &:not(:first-child) {
-      border-top: 1px solid var(--color-divider);
-      padding-top: 2rem;
-    }
-  `;
-
+export default function Section({ id, children }) {
   return <SectionSlyte id={id}>{children}</SectionSlyte>;
 }
 
 Section.propTypes = {
   id: PropTypes.string,
   children: PropTypes.node.isRequired,
-  sx: PropTypes.string,
 };
 
 Section.defaultProps = {
   id: '',
-  sx: '',
 };
+
+const SectionSlyte = styled.section`
+  margin-bottom: 2rem;
+
+  &:not(:first-child) {
+    border-top: 1px solid var(--color-divider);
+    padding-top: 2rem;
+  }
+
+  &[id='quizz'] {
+    background-color: #ffcf33;
+    padding: 2rem;
+    min-width: 100vw;
+    margin-left: calc((100vw - 100%) / -2);
+  }
+`;
