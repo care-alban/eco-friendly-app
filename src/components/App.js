eef2b857a40635b192b595f9da34b29b92e6ff4a
Alban Caré <alban.care@gmail.com>
2023-03-15 18:40:15 +0100

    refact: code prettier

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 
 import HomePage from '../pages/HomePage';
+import CategoryPage from '../pages/CategoryPage';
 
 import { getAllCategories } from '../actions/commonActions';
 import { getArticles } from '../actions/articlesActions';
@@ -17,33 +18,24 @@ function App() {
   const advices = useSelector((state) => state.advices.list);
 
   useEffect(() => {
-    if (categories.length === 0) {
-      dispatch(getAllCategories());
-    }
-    if (articles.length === 0) {
-      dispatch(
-        getArticles([
-          { name: 'limit', value: 18 },
-          { name: 'sorttype', value: 'created_at' },
-          { name: 'order', value: 'desc' },
-        ]),
-      );
-    }
-    if (advices.length === 0) {
-      dispatch(
-        getAdvices([
-          { name: 'limit', value: 8 },
-          { name: 'sorttype', value: 'created_at' },
-          { name: 'order', value: 'desc' },
-        ]),
-      );
-    }
+    const params = [
+      { name: 'limit', value: 8 },
+      { name: 'sorttype', value: 'created_at' },
+      { name: 'order', value: 'desc' },
+    ];
+    if (categories.length === 0) dispatch(getAllCategories());
+    if (articles.length === 0) dispatch(getArticles(params));
+    if (advices.length === 0) dispatch(getAdvices(params));
   }, [categories, articles, advices]);
 
   return (
     <Routes>
       <Route>
         <Route path={`${config.basePath}`} element={<HomePage />} />
+        <Route
+          path={`${config.basePath}/categories/:name`}
+          element={<CategoryPage />}
+        />
       </Route>
     </Routes>
   );
