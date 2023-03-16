ee3c3d7dfcbed8cc18ed528b05159cdfbeb810c7
Alban Caré <alban.care@gmail.com>
2023-03-16 22:34:21 +0100

    feat: add functions to mix array and filter value

--- src/utils/index.js
+++ src/utils/index.js
@@ -24,4 +24,44 @@ const getArrayItems = (array, start, number) => {
   return null;
 };
 
-export { getFisrtArrayItem, getArrayItems };
+/**
+ * @name randomlyMixSeveralArrays
+ * @description Randomly mix several arrays
+ * @param {Array} arrays Arrays to mix
+ * @param {Number} number Number of items to get if 0 retun all the items
+ * @param {Number} start Start index
+ * @returns {Array} Array of items
+ */
+const randomlyMixSeveralArrays = (arrays, number = 0, start = 0) => {
+  const items = [];
+  arrays.forEach((array) => {
+    items.push(...array);
+  });
+  const shuffled = items.sort(() => 0.5 - Math.random());
+  if (number > 0) {
+    return shuffled.slice(start, start + number);
+  }
+  return shuffled;
+};
+
+/**
+ * @name searchValueInArray
+ * @description Search a value in an array
+ * @param {Array} array Array to search in
+ * @param {String} value Value to search
+ * @param {String} key Key to search in
+ * @returns {Array} Array of items
+ */
+const searchValueInArray = (array, value, key) => {
+  if (array.length > 0) {
+    return array.filter((item) => item[key] === value);
+  }
+  return null;
+};
+
+export {
+  getFisrtArrayItem,
+  getArrayItems,
+  randomlyMixSeveralArrays,
+  searchValueInArray,
+};
