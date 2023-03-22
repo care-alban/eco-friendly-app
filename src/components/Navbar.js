6a2f30d112626b91c138176e971dd0af033ca14d
Alban Caré <alban.care@gmail.com>
2023-03-22 01:00:33 +0100

    refact: upgrade navbar (sticky menu)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,6 +19,22 @@ import Avatar from '@mui/material/Avatar';
 import { getAllCategories } from '../actions/commonActions';
 
 // const pages = ['Maison', 'Santé', 'Bien-être', 'Alimentation'];
+const link = {
+  mr: 3,
+  fontFamily: 'monospace',
+  fontWeight: 500,
+  letterSpacing: '.3rem',
+  color: '#000',
+  textDecoration: 'none',
+  borderBottom: 1,
+  borderColor: 'transparent',
+  '&:hover': {
+    borderColor: 'inherit',
+  },
+  '&:last-child': {
+    mr: 0,
+  },
+};
 
 export default function Navbar() {
   const dispatch = useDispatch();
@@ -30,141 +46,119 @@ export default function Navbar() {
   }, [categories]);
 
   return (
-    <AppBar>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <LocalFloristIcon
-            sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}
-          />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            Eco-Friendly
-          </Typography>
-          <MobileNav categories={categories} user={user} />
-          <Box
-            sx={{
-              flexGrow: 1,
-              display: 'flex',
-              flexWrap: 'nowrap',
-              justifyContent: 'center',
-            }}
-          >
+    <>
+      <AppBar position="relative">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
             <LocalFloristIcon
-              sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }}
+              sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}
             />
             <Typography
               variant="h6"
               noWrap
+              component="a"
+              href="/"
               sx={{
-                display: { xs: 'flex', md: 'none' },
-                flexGrow: 1,
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
                 fontFamily: 'monospace',
-                fontWeight: 500,
+                fontWeight: 700,
                 letterSpacing: '.3rem',
                 color: 'inherit',
+                textDecoration: 'none',
               }}
             >
               Eco-Friendly
             </Typography>
-          </Box>
-          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-            {user ? (
-              <UserMenu />
-            ) : (
-              <>
-                <Button
-                  component={RouterLink}
-                  to="/connexion"
-                  color="inherit"
-                  size="small"
-                  sx={{ marginRight: '1rem' }}
-                >
-                  S'identifier
-                </Button>
-                <Button
-                  component={RouterLink}
-                  to="/inscription"
-                  color="inherit"
-                  size="small"
-                  variant="outlined"
-                >
-                  S'inscrire
-                </Button>
-              </>
-            )}
+            <MobileNav categories={categories} user={user} />
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: 'flex',
+                flexWrap: 'nowrap',
+                justifyContent: 'center',
+              }}
+            >
+              <LocalFloristIcon
+                sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }}
+              />
+              <Typography
+                variant="h6"
+                noWrap
+                sx={{
+                  display: { xs: 'flex', md: 'none' },
+                  flexGrow: 1,
+                  fontFamily: 'monospace',
+                  fontWeight: 500,
+                  letterSpacing: '.3rem',
+                  color: 'inherit',
+                }}
+              >
+                Eco-Friendly
+              </Typography>
+            </Box>
+            <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+              {user ? (
+                <UserMenu />
+              ) : (
+                <>
+                  <Button
+                    component={RouterLink}
+                    to="/connexion"
+                    color="inherit"
+                    size="small"
+                    sx={{ marginRight: '1rem' }}
+                  >
+                    S'identifier
+                  </Button>
+                  <Button
+                    component={RouterLink}
+                    to="/inscription"
+                    color="inherit"
+                    size="small"
+                    variant="outlined"
+                  >
+                    S'inscrire
+                  </Button>
+                </>
+              )}
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <AppBar position="sticky">
+        <Toolbar disableGutters>
+          <Box
+            sx={{
+              flexGrow: 1,
+              py: 3,
+              display: { xs: 'none', md: 'flex' },
+              justifyContent: 'center',
+              borderBottom: 1,
+              borderColor: 'divider',
+              backgroundColor: 'white',
+            }}
+          >
+            <Link component={RouterLink} to="/" color="inherit" sx={link}>
+              Actualité
+            </Link>
+            {categories.map((category) => (
+              <Link
+                key={category.id}
+                component={RouterLink}
+                to={`/categories/${category.slug}`}
+                color="inherit"
+                sx={link}
+              >
+                {category.name}
+              </Link>
+            ))}
           </Box>
         </Toolbar>
-      </Container>
-      <AppMenu categories={categories} />
-    </AppBar>
+      </AppBar>
+    </>
   );
 }
-
-/* desktop menu */
-function AppMenu({ categories }) {
-  const link = {
-    mr: 3,
-    fontFamily: 'monospace',
-    fontWeight: 500,
-    letterSpacing: '.3rem',
-    color: '#000',
-    textDecoration: 'none',
-    borderBottom: 1,
-    borderColor: 'transparent',
-    '&:hover': {
-      borderColor: 'inherit',
-    },
-    '&:last-child': {
-      mr: 0,
-    },
-  };
-
-  return (
-    <Box
-      sx={{
-        flexGrow: 1,
-        py: 3,
-        display: { xs: 'none', md: 'flex' },
-        justifyContent: 'center',
-        borderBottom: 1,
-        borderColor: 'divider',
-        backgroundColor: 'white',
-      }}
-    >
-      <Link component={RouterLink} to="/" color="inherit" sx={link}>
-        Actualité
-      </Link>
-      {categories.map((category) => (
-        <Link
-          key={category.id}
-          component={RouterLink}
-          to={`/categories/${category.slug}`}
-          color="inherit"
-          sx={link}
-        >
-          {category.name}
-        </Link>
-      ))}
-    </Box>
-  );
-}
-
-AppMenu.propTypes = {
-  categories: PropTypes.array.isRequired,
-};
 
 function MobileNav({ categories, user }) {
   const [anchorElNav, setAnchorElNav] = useState(null);
@@ -205,6 +199,10 @@ function MobileNav({ categories, user }) {
         onClose={handleCloseNavMenu}
         sx={{
           display: { xs: 'block', md: 'none' },
+          '& .MuiPaper-root': {
+            width: '100%',
+            maxWidth: '100%',
+          },
         }}
       >
         <MenuItem onClick={handleCloseNavMenu}>
