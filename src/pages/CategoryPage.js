4b673aeae2b13f5ee18fb7ea0afc22fb66f1bc14
Alban Caré <alban.care@gmail.com>
2023-03-23 23:40:25 +0100

    feat: upgrade advices card

--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -1,24 +1,29 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Link as RouterLink, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+
 import styled from '@emotion/styled';
 import {
   Box,
+  Button,
+  CardActions,
   CardActionArea,
   CardContent,
   CardMedia,
   Grid,
+  IconButton,
   Typography,
 } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-import GroupsIcon from '@mui/icons-material/Groups';
 import { Hero, Layout, Loader, SearchBar } from '../components';
 import MediumCard from '../components/Cards/MediumCard';
 
 import { getArticles } from '../actions/articlesActions';
 import { getAdvices } from '../actions/advicesActions';
 import { searchBarOnChange } from '../actions/commonActions';
-import { randomlyMixSeveralArrays } from '../utils/index';
 
 export default function CategoryPage() {
   const dispatch = useDispatch();
@@ -41,43 +46,18 @@ export default function CategoryPage() {
         { name: 'sorttype', value: 'created_at' },
         { name: 'order', value: 'desc' },
       ];
-      /**
-       * Get all the articles and all the advices of the category
-       * TODO: add a limit number and get the articles and advices on scroll
-       * */
       dispatch(getArticles(params));
       dispatch(getAdvices(params));
     }
   }, [category]);
 
-  const [allArticlesAndAdvices, setAllArticlesAndAdvices] = useState([]);
+  /* Get the last 8 articles and last 8 advices from the store */
+  /* TODO: Install a pagination system */
   const articles = useSelector((state) => state.articles.list);
   const advices = useSelector((state) => state.advices.list);
+  const userId = useSelector((state) => state.user.data.id);
 
-  /* Mix the articles and advices and filter them by category */
-  useEffect(() => {
-    if (articles.length > 0 && advices.length > 0) {
-      setAllArticlesAndAdvices(
-        randomlyMixSeveralArrays([articles, advices], 0, 0).filter(
-          (item) => item.category.id === category.id,
-        ),
-      );
-    }
-  }, [articles, advices, category]);
-
-  /* Truncate the content of the card */
-  const TruncateContent = styled.div`
-    overflow: hidden;
-    overflow-wrap: break-word;
-    text-overflow: ellipsis;
-    display: -webkit-box;
-    -webkit-line-clamp: 5;
-    -webkit-box-orient: vertical;
-    -webkit-hyphens: auto;
-    hyphens: auto;
-  `;
-
-  if (!category || !allArticlesAndAdvices.length > 0) {
+  if (!category) {
     return (
       <Layout>
         <Loader />
@@ -92,97 +72,272 @@ export default function CategoryPage() {
         title={category.name}
         subtitle={category.tagline}
       />
-      <Box sx={{ flexGrow: 1, marginY: 2, marginX: 0 }}>
-        <SearchBar list={allArticlesAndAdvices} keys={['title', 'content']} />
-      </Box>
-      <section>
+      <section id="articles">
+        <Box sx={{ flexGrow: 1, marginY: 2, marginX: 0 }}>
+          <SearchBar list={articles} keys={['title', 'content']} />
+        </Box>
+        <Typography
+          variant="h4"
+          component="h2"
+          color="inherit"
+          sx={{ marginY: 2 }}
+        >
+          Articles
+        </Typography>
         <Grid container spacing={2}>
-          {allArticlesAndAdvices.map((item) => (
+          {articles.map((item) => (
             <Grid item xs={12} sm={6} md={4} lg={3} key={`${item.title}`}>
-              <MediumCard
-                sx={{
-                  boder: 1,
-                  maxWidth: { xs: '100%', md: '100%' },
-                  boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px',
-                }}
-              >
-                <CardActionArea
-                  LinkComponent={RouterLink}
-                  to={`/articles/${item.slug}`}
-                >
-                  {item.picture ? (
-                    <CardMedia
-                      component="img"
-                      height="200"
-                      image={item.picture}
-                      alt={item.title}
-                    />
-                  ) : (
-                    <CardMedia
-                      height="200"
-                      sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        minHeight: 200,
-                      }}
-                    >
-                      <GroupsIcon
-                        sx={{
-                          fontSize: 72,
-                        }}
-                      />
-                      <Typography variant="h6" component="h6" marginTop="-1rem">
-                        {item.contributor.nickname}
-                      </Typography>
-                    </CardMedia>
-                  )}
-                  <CardContent
-                    sx={{
-                      display: 'flex',
-                      flexDirection: 'column',
-                      justifyContent: 'space-between',
-                      minHeight: 280,
-                    }}
-                  >
-                    <Box>
-                      <Typography gutterBottom variant="h6" component="div">
-                        {item.title}
-                      </Typography>
-                      {item.author && (
-                        <Typography
-                          variant="body2"
-                          color="text.secondary"
-                          component="h6"
-                        >
-                          Redigé par {item.author.nickname}
-                        </Typography>
-                      )}
-                      <Typography
-                        variant="body2"
-                        color="text.secondary"
-                        component="span"
-                      >
-                        {item.updated_at
-                          ? `Mis à jour le ${new Date(
-                              item.updated_at,
-                            ).toLocaleDateString()}`
-                          : `Publié le ${new Date(
-                              item.created_at,
-                            ).toLocaleDateString()}`}
-                      </Typography>
-                    </Box>
-                    <TruncateContent
-                      dangerouslySetInnerHTML={{ __html: item.content }}
-                    />
-                  </CardContent>
-                </CardActionArea>
-              </MediumCard>
+              <ArticlesMediumCard item={item} />
             </Grid>
           ))}
         </Grid>
+        <Box
+          sx={{
+            flexGrow: 1,
+            marginY: 2,
+            marginX: 0,
+            display: 'flex',
+            justifyContent: 'flex-end',
+          }}
+        >
+          <Button variant="contained">Voir plus d'articles...</Button>
+        </Box>
+      </section>
+      <section id="advices">
+        <Typography
+          variant="h4"
+          component="h2"
+          color="inherit"
+          sx={{ marginY: 2 }}
+        >
+          Conseils
+        </Typography>
+        <Grid container spacing={2}>
+          {advices.map((advice) => (
+            <Grid item xs={12} md={6} key={`${advice.id}`}>
+              <AdvicesMediumCard advice={advice} userId={userId} />
+            </Grid>
+          ))}
+        </Grid>
+        <Box
+          sx={{
+            flexGrow: 1,
+            marginY: 2,
+            marginX: 0,
+            display: 'flex',
+            justifyContent: 'flex-end',
+          }}
+        >
+          <Button variant="contained">Voir plus de conseils...</Button>
+        </Box>
       </section>
     </Layout>
   );
 }
+
+function ArticlesMediumCard({ item }) {
+  /* Truncate the content of the card */
+  const TruncateContent = styled.div`
+    overflow: hidden;
+    overflow-wrap: break-word;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+    -webkit-hyphens: auto;
+    hyphens: auto;
+  `;
+
+  return (
+    <MediumCard
+      sx={{
+        boder: 1,
+        maxWidth: { xs: '100%', md: '100%' },
+        boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px',
+      }}
+    >
+      <CardActionArea LinkComponent={RouterLink} to={`/articles/${item.slug}`}>
+        <CardMedia
+          component="img"
+          height="200"
+          image={item.picture}
+          alt={item.title}
+        />
+        <CardContent
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            minHeight: 280,
+          }}
+        >
+          <Box>
+            <Typography gutterBottom variant="h6" component="div">
+              {item.title}
+            </Typography>
+            {item.author && (
+              <Typography variant="body2" color="text.secondary" component="h6">
+                Redigé par {item.author.nickname}
+              </Typography>
+            )}
+            <Typography variant="body2" color="text.secondary" component="span">
+              {item.updated_at
+                ? `Mis à jour le ${new Date(
+                    item.updated_at,
+                  ).toLocaleDateString()}`
+                : `Publié le ${new Date(item.created_at).toLocaleDateString()}`}
+            </Typography>
+          </Box>
+          <TruncateContent dangerouslySetInnerHTML={{ __html: item.content }} />
+        </CardContent>
+      </CardActionArea>
+    </MediumCard>
+  );
+}
+
+ArticlesMediumCard.propTypes = {
+  item: PropTypes.shape({
+    slug: PropTypes.string.isRequired,
+    picture: PropTypes.string,
+    contributor: PropTypes.shape({
+      id: PropTypes.number,
+      nickname: PropTypes.string,
+    }),
+    title: PropTypes.string.isRequired,
+    author: PropTypes.shape({
+      nickname: PropTypes.string,
+    }),
+    updated_at: PropTypes.string,
+    created_at: PropTypes.string,
+    content: PropTypes.string.isRequired,
+  }),
+};
+
+ArticlesMediumCard.defaultProps = {
+  item: {
+    picture: null,
+    contributor: {
+      id: null,
+      nickname: '',
+    },
+    author: {
+      nickname: '',
+    },
+    created_at: '',
+    updated_at: '',
+  },
+};
+
+function AdvicesMediumCard({ advice, userId }) {
+  /* Truncate the content of the card */
+  const TruncateContent = styled.div`
+    overflow: hidden;
+    overflow-wrap: break-word;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+    -webkit-hyphens: auto;
+    hyphens: auto;
+  `;
+
+  return (
+    <MediumCard
+      sx={{
+        boder: 1,
+        maxWidth: { xs: '100%', md: '100%' },
+        boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px',
+      }}
+    >
+      <CardContent
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Box>
+          {userId === advice.contributor.id && (
+            <CardActions
+              sx={{
+                float: 'right',
+              }}
+            >
+              <IconButton aria-label="delete">
+                <DeleteIcon />
+              </IconButton>
+              <IconButton aria-label="edit" color="primary">
+                <EditIcon />
+              </IconButton>
+            </CardActions>
+          )}
+          <Typography gutterBottom variant="h6" component="div">
+            {advice.title}
+          </Typography>
+          {advice.contributor && (
+            <Typography variant="body2" color="text.secondary" component="h6">
+              Proposé par{' '}
+              {userId === advice.contributor.id
+                ? 'vous'
+                : advice.contributor.nickname}
+            </Typography>
+          )}
+          <Typography variant="body2" color="text.secondary" component="span">
+            {advice.updated_at
+              ? `Mis à jour le ${new Date(
+                  advice.updated_at,
+                ).toLocaleDateString()}`
+              : `Publié le ${new Date(advice.created_at).toLocaleDateString()}`}
+          </Typography>
+        </Box>
+        <TruncateContent dangerouslySetInnerHTML={{ __html: advice.content }} />
+        <CardActions
+          sx={{
+            display: 'flex',
+            marginTop: 2,
+            justifyContent: { xs: 'center', md: 'flex-end' },
+          }}
+        >
+          <Button
+            component={RouterLink}
+            to={`/conseils/${advice.slug}`}
+            color="secondary"
+            size="small"
+            variant="outlined"
+          >
+            En savoir plus
+          </Button>
+        </CardActions>
+      </CardContent>
+    </MediumCard>
+  );
+}
+
+AdvicesMediumCard.propTypes = {
+  advice: PropTypes.shape({
+    slug: PropTypes.string.isRequired,
+    picture: PropTypes.string,
+    contributor: PropTypes.shape({
+      id: PropTypes.number,
+      nickname: PropTypes.string,
+    }),
+    title: PropTypes.string.isRequired,
+    updated_at: PropTypes.string,
+    created_at: PropTypes.string,
+    content: PropTypes.string.isRequired,
+  }),
+  userId: PropTypes.number,
+};
+
+AdvicesMediumCard.defaultProps = {
+  advice: {
+    picture: null,
+    contributor: {
+      id: null,
+      nickname: '',
+    },
+    created_at: '',
+    updated_at: '',
+  },
+  userId: null,
+};
