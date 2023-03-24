aa2254aee5515149aaca7ed9959b9071970b5b82
Alban Caré <alban.care@gmail.com>
2023-03-24 18:17:51 +0100

    refact: remove edit and remove advice controllers

--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -7,19 +7,15 @@ import styled from '@emotion/styled';
 import {
   Box,
   Button,
-  CardActions,
   CardActionArea,
   CardContent,
   CardMedia,
   Grid,
-  IconButton,
   Typography,
 } from '@mui/material';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
 
 import { Hero, Layout, Loader, SearchBar } from '../components';
-import MediumCard from '../components/Cards/MediumCard';
+import { MediumCard, AdvicesMediumCard } from '../components/Cards';
 
 import { getArticles } from '../actions/articlesActions';
 import { getAdvices } from '../actions/advicesActions';
@@ -51,11 +47,12 @@ export default function CategoryPage() {
     }
   }, [category]);
 
-  /* Get the last 8 articles and last 8 advices from the store */
-  /* TODO: Install a pagination system */
-  const articles = useSelector((state) => state.articles.list);
-  const advices = useSelector((state) => state.advices.list);
-  const userId = useSelector((state) => state.user.data.id);
+  const articles = useSelector((state) => state.articles.list).filter(
+    (item) => item.category.id === category.id,
+  );
+  const advices = useSelector((state) => state.advices.list).filter(
+    (item) => item.category.id === category.id,
+  );
 
   if (!category) {
     return (
@@ -115,7 +112,7 @@ export default function CategoryPage() {
         <Grid container spacing={2}>
           {advices.map((advice) => (
             <Grid item xs={12} md={6} key={`${advice.id}`}>
-              <AdvicesMediumCard advice={advice} userId={userId} />
+              <AdvicesMediumCard advice={advice} />
             </Grid>
           ))}
         </Grid>
@@ -226,118 +223,4 @@ ArticlesMediumCard.defaultProps = {
     created_at: '',
     updated_at: '',
   },
-};
-
-function AdvicesMediumCard({ advice, userId }) {
-  /* Truncate the content of the card */
-  const TruncateContent = styled.div`
-    overflow: hidden;
-    overflow-wrap: break-word;
-    text-overflow: ellipsis;
-    display: -webkit-box;
-    -webkit-line-clamp: 3;
-    -webkit-box-orient: vertical;
-    -webkit-hyphens: auto;
-    hyphens: auto;
-  `;
-
-  return (
-    <MediumCard
-      sx={{
-        boder: 1,
-        maxWidth: { xs: '100%', md: '100%' },
-        boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px',
-      }}
-    >
-      <CardContent
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-between',
-        }}
-      >
-        <Box>
-          {userId === advice.contributor.id && (
-            <CardActions
-              sx={{
-                float: 'right',
-              }}
-            >
-              <IconButton aria-label="delete">
-                <DeleteIcon />
-              </IconButton>
-              <IconButton aria-label="edit" color="primary">
-                <EditIcon />
-              </IconButton>
-            </CardActions>
-          )}
-          <Typography gutterBottom variant="h6" component="div">
-            {advice.title}
-          </Typography>
-          {advice.contributor && (
-            <Typography variant="body2" color="text.secondary" component="h6">
-              Proposé par{' '}
-              {userId === advice.contributor.id
-                ? 'vous'
-                : advice.contributor.nickname}
-            </Typography>
-          )}
-          <Typography variant="body2" color="text.secondary" component="span">
-            {advice.updated_at
-              ? `Mis à jour le ${new Date(
-                  advice.updated_at,
-                ).toLocaleDateString()}`
-              : `Publié le ${new Date(advice.created_at).toLocaleDateString()}`}
-          </Typography>
-        </Box>
-        <TruncateContent dangerouslySetInnerHTML={{ __html: advice.content }} />
-        <CardActions
-          sx={{
-            display: 'flex',
-            marginTop: 2,
-            justifyContent: { xs: 'center', md: 'flex-end' },
-          }}
-        >
-          <Button
-            component={RouterLink}
-            to={`/conseils/${advice.slug}`}
-            color="secondary"
-            size="small"
-            variant="outlined"
-          >
-            En savoir plus
-          </Button>
-        </CardActions>
-      </CardContent>
-    </MediumCard>
-  );
-}
-
-AdvicesMediumCard.propTypes = {
-  advice: PropTypes.shape({
-    slug: PropTypes.string.isRequired,
-    picture: PropTypes.string,
-    contributor: PropTypes.shape({
-      id: PropTypes.number,
-      nickname: PropTypes.string,
-    }),
-    title: PropTypes.string.isRequired,
-    updated_at: PropTypes.string,
-    created_at: PropTypes.string,
-    content: PropTypes.string.isRequired,
-  }),
-  userId: PropTypes.number,
-};
-
-AdvicesMediumCard.defaultProps = {
-  advice: {
-    picture: null,
-    contributor: {
-      id: null,
-      nickname: '',
-    },
-    created_at: '',
-    updated_at: '',
-  },
-  userId: null,
 };
