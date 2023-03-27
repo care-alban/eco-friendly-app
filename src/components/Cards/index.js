929f0912f49fd2fb5de1267fb8786b9f85dbffd6
Alban Caré <alban.care@gmail.com>
2023-03-27 17:12:27 +0200

    feat: add articles default card component

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,4 +1,5 @@
 export { default as AdvicesMediumCard } from './AdvicesMediumCard';
+export { default as ArticlesMediumCard } from './ArticlesMediumCard';
 export { default as largeCard } from './LargeCard';
 export { default as MediumCard } from './MediumCard';
 export { default as SmallCard } from './SmallCard';
