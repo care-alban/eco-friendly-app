09878045b24a388f5aa713e94d21d5be21253cd7
Alban Caré <alban.care@gmail.com>
2023-03-27 17:13:19 +0200

    refact: clean up

--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -1,21 +1,11 @@
 import { useEffect } from 'react';
 import { Link as RouterLink, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import PropTypes from 'prop-types';
 
-import styled from '@emotion/styled';
-import {
-  Box,
-  Button,
-  CardActionArea,
-  CardContent,
-  CardMedia,
-  Grid,
-  Typography,
-} from '@mui/material';
+import { Box, Button, Grid, Typography } from '@mui/material';
 
 import { Hero, Layout, Loader, SearchBar } from '../components';
-import { MediumCard, AdvicesMediumCard } from '../components/Cards';
+import { ArticlesMediumCard, AdvicesMediumCard } from '../components/Cards';
 
 import { getArticles } from '../actions/articlesActions';
 import { getAdvices } from '../actions/advicesActions';
@@ -82,9 +72,9 @@ export default function CategoryPage() {
           Articles
         </Typography>
         <Grid container spacing={2}>
-          {articles.map((item) => (
-            <Grid item xs={12} sm={6} md={4} lg={3} key={`${item.title}`}>
-              <ArticlesMediumCard item={item} />
+          {articles.map((article) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} key={`${article.id}`}>
+              <ArticlesMediumCard article={article} />
             </Grid>
           ))}
         </Grid>
@@ -97,7 +87,9 @@ export default function CategoryPage() {
             justifyContent: 'flex-end',
           }}
         >
-          <Button variant="contained">Voir plus d'articles...</Button>
+          <Button component={RouterLink} to="/articles" variant="contained">
+            Voir plus d'articles...
+          </Button>
         </Box>
       </section>
       <section id="advices">
@@ -131,96 +123,3 @@ export default function CategoryPage() {
     </Layout>
   );
 }
-
-function ArticlesMediumCard({ item }) {
-  /* Truncate the content of the card */
-  const TruncateContent = styled.div`
-    overflow: hidden;
-    overflow-wrap: break-word;
-    text-overflow: ellipsis;
-    display: -webkit-box;
-    -webkit-line-clamp: 3;
-    -webkit-box-orient: vertical;
-    -webkit-hyphens: auto;
-    hyphens: auto;
-  `;
-
-  return (
-    <MediumCard
-      sx={{
-        boder: 1,
-        maxWidth: { xs: '100%', md: '100%' },
-        boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px',
-      }}
-    >
-      <CardActionArea LinkComponent={RouterLink} to={`/articles/${item.slug}`}>
-        <CardMedia
-          component="img"
-          height="200"
-          image={item.picture}
-          alt={item.title}
-        />
-        <CardContent
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-            minHeight: 280,
-          }}
-        >
-          <Box>
-            <Typography gutterBottom variant="h6" component="div">
-              {item.title}
-            </Typography>
-            {item.author && (
-              <Typography variant="body2" color="text.secondary" component="h6">
-                Redigé par {item.author.nickname}
-              </Typography>
-            )}
-            <Typography variant="body2" color="text.secondary" component="span">
-              {item.updated_at
-                ? `Mis à jour le ${new Date(
-                    item.updated_at,
-                  ).toLocaleDateString()}`
-                : `Publié le ${new Date(item.created_at).toLocaleDateString()}`}
-            </Typography>
-          </Box>
-          <TruncateContent dangerouslySetInnerHTML={{ __html: item.content }} />
-        </CardContent>
-      </CardActionArea>
-    </MediumCard>
-  );
-}
-
-ArticlesMediumCard.propTypes = {
-  item: PropTypes.shape({
-    slug: PropTypes.string.isRequired,
-    picture: PropTypes.string,
-    contributor: PropTypes.shape({
-      id: PropTypes.number,
-      nickname: PropTypes.string,
-    }),
-    title: PropTypes.string.isRequired,
-    author: PropTypes.shape({
-      nickname: PropTypes.string,
-    }),
-    updated_at: PropTypes.string,
-    created_at: PropTypes.string,
-    content: PropTypes.string.isRequired,
-  }),
-};
-
-ArticlesMediumCard.defaultProps = {
-  item: {
-    picture: null,
-    contributor: {
-      id: null,
-      nickname: '',
-    },
-    author: {
-      nickname: '',
-    },
-    created_at: '',
-    updated_at: '',
-  },
-};
