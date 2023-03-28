89fb01eb7a343d429fee87b694f7a782ffe7ff19
Alban Caré <alban.care@gmail.com>
2023-03-28 12:05:46 +0200

    refact: no longer loads data by default

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,3 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 
 import HomePage from '../pages/HomePage';
@@ -15,24 +13,7 @@ import ValidationPage from '../pages/ValidationPage';
 import LegalNoticePage from '../pages/LegalNoticePage';
 import NotFoundPage from '../pages/NotFoundPage';
 
-import { getArticles } from '../actions/articlesActions';
-import { getAdvices } from '../actions/advicesActions';
-
 function App() {
-  const dispatch = useDispatch();
-  const articles = useSelector((state) => state.articles.list);
-  const advices = useSelector((state) => state.advices.list);
-
-  useEffect(() => {
-    const params = [
-      { name: 'limit', value: 8 },
-      { name: 'sorttype', value: 'created_at' },
-      { name: 'order', value: 'desc' },
-    ];
-    if (articles.length === 0) dispatch(getArticles(params));
-    if (advices.length === 0) dispatch(getAdvices(params));
-  }, [articles, advices]);
-
   return (
     <Routes>
       <Route>
