c9cb9ca50a17e5be82ed2c5969c57a3e684e9742
Alban Caré <alban.care@gmail.com>
2023-03-29 10:44:57 +0200

    refact: upgrade design

--- src/pages/UserProfilePage.js
+++ src/pages/UserProfilePage.js
@@ -1,7 +1,16 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { Avatar, Badge, Box, Button, Typography, Grid } from '@mui/material';
+import {
+  Avatar,
+  Badge,
+  Box,
+  Button,
+  Grid,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material';
 import { amber } from '@mui/material/colors';
 import { styled } from '@mui/material/styles';
 
@@ -30,6 +39,11 @@ export default function UserProfilePage() {
       </Layout>
     );
   }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('submit : ', nickname, email, firstname, lastname);
+  };
 
   /* styles */
   const StyledBadge = styled(Badge)(({ theme }) => ({
@@ -64,102 +78,114 @@ export default function UserProfilePage() {
           md={6}
           sx={{ position: 'relative', padding: '1rem', paddingTop: '3rem' }}
         >
-          <StyledBadge
-            overlap="circular"
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-            }}
-          >
-            <Avatar
-              alt={`avatar de ${nickname}`}
-              src={avatar}
-              sx={{
-                width: '6rem',
-                height: '6rem',
-                mr: 2,
-                border: 2,
-                borderColor: 'neutral.main',
-              }}
-            />
-            <Button
-              variant="text"
-              color="primary"
-              // onClick={toggleModal}
-              name="avatar"
-            >
-              Changer d'avatar
-            </Button>
-          </StyledBadge>
-          <StyleRow>
-            <Box
+          <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+            <StyledBadge
+              overlap="circular"
               sx={{
                 display: 'flex',
-                flexDirection: 'column',
+                alignItems: 'center',
               }}
             >
-              <Typography variant="h5" component="h5">
-                Pseudo
-              </Typography>
-              <Typography variant="body1" component="p">
-                {nickname}
-              </Typography>
-            </Box>
-            <Button
-              variant="contained"
-              color="primary"
-              // onClick={toggleModal}
-              name="nickname"
+              <Avatar
+                alt={`avatar de ${nickname}`}
+                src={avatar}
+                sx={{
+                  width: '6rem',
+                  height: '6rem',
+                  mr: 2,
+                  border: 2,
+                  borderColor: 'neutral.main',
+                }}
+              />
+              <Button variant="text" color="primary" name="avatar">
+                Changer d'avatar
+              </Button>
+            </StyledBadge>
+            <StyleRow>
+              <TextField
+                name="nickname"
+                type="nickname"
+                label="Pseudo"
+                placeholder="Pseudo"
+                variant="outlined"
+                value={nickname}
+                sx={{ flexGrow: 1 }}
+                // onChange={changeField}
+              />
+            </StyleRow>
+            <StyleRow>
+              <TextField
+                name="email"
+                type="email"
+                label="Email"
+                placeholder="Email"
+                variant="outlined"
+                value={email}
+                sx={{ flexGrow: 1 }}
+                // onChange={changeField}
+              />
+            </StyleRow>
+            <StyleRow>
+              <Box
+                sx={{
+                  flexGrow: 1,
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                }}
+              >
+                <TextField
+                  name="lastname"
+                  type="lastname"
+                  label="Nom de famille"
+                  placeholder="Nom de famille"
+                  variant="outlined"
+                  value={firstname}
+                  sx={{
+                    flexGrow: '1',
+                    mr: 1,
+                  }}
+                  // onChange={changeField}
+                />
+                <TextField
+                  name="firstname"
+                  type="firstname"
+                  label="Prénom"
+                  placeholder="Prénom"
+                  variant="outlined"
+                  value={lastname}
+                  sx={{
+                    flexGrow: '1',
+                    ml: 1,
+                  }}
+                  // onChange={changeField}
+                />
+              </Box>
+            </StyleRow>
+            <Stack
+              spacing={2}
+              direction="row"
+              marginTop={2}
+              sx={{ display: 'flex', justifyContent: 'flex-end' }}
             >
-              Modifier
-            </Button>
-          </StyleRow>
-          <StyleRow>
-            <Box
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-              }}
-            >
-              <Typography variant="h5" component="h5">
-                Email
-              </Typography>
-              <Typography variant="body1" component="p">
-                {email}
-              </Typography>
-            </Box>
-            <Button
-              variant="contained"
-              color="primary"
-              // onClick={toggleModal}
-              name="email"
-            >
-              Modifier
-            </Button>
-          </StyleRow>
-          <StyleRow>
-            <Box
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-              }}
-            >
-              <Typography variant="h5" component="h5">
-                Nom et Prénom
-              </Typography>
-              <Typography variant="body1" component="p">
-                {firstname} {lastname}
-              </Typography>
-            </Box>
-            <Button
-              variant="contained"
-              color="primary"
-              // onClick={toggleModal}
-              name="fullname"
-            >
-              Modifier
-            </Button>
-          </StyleRow>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                name="nickname"
+              >
+                Mettre à jour
+              </Button>
+              <Button
+                type="button"
+                variant="outlined"
+                color="secondary"
+                name="cancel"
+                ml={2}
+              >
+                Réinitialiser
+              </Button>
+            </Stack>
+          </form>
         </Grid>
         <Grid
           item
@@ -170,8 +196,8 @@ export default function UserProfilePage() {
           <Button
             variant="contained"
             color="primary"
-            // onClick={toggleModal}
             name="password"
+            // onClick={handlePasswordUpdate}
           >
             Changer de mot de passe
           </Button>
@@ -186,8 +212,8 @@ export default function UserProfilePage() {
             <Button
               variant="contained"
               color="secondary"
-              // onClick={toggleModal}
               name="delete-account"
+              // onClick={handleRemoveAccount}
               sx={{ marginBottom: 2 }}
             >
               Supprimer mon compte
@@ -198,18 +224,22 @@ export default function UserProfilePage() {
           </Box>
         </Grid>
       </Grid>
-      <Typography variant="h4" component="h4" my={4}>
-        {advices.length > 0
-          ? `Félicitations ! Vous avez déjà créé ${advices.length} conseil(s)`
-          : "Vous n'avez pas encore créé de conseils"}
-      </Typography>
-      <Grid container spacing={2}>
-        {advices.map((advice) => (
-          <Grid item xs={12} sm={6} md={4} lg={3} key={`${advice.id}`}>
-            <AdvicesMediumCard advice={advice} />
+      {advices && (
+        <>
+          <Typography variant="h4" component="h4" my={4}>
+            {advices.length > 0
+              ? `Félicitations ! Vous avez déjà créé ${advices.length} conseil(s)`
+              : "Vous n'avez pas encore créé de conseils"}
+          </Typography>
+          <Grid container spacing={2}>
+            {advices.map((advice) => (
+              <Grid item xs={12} sm={6} md={4} lg={3} key={`${advice.id}`}>
+                <AdvicesMediumCard advice={advice} />
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
+        </>
+      )}
     </Layout>
   );
 }
