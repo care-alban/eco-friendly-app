4fa4afef4dfbf8098439d066f26a478148dfd08f
Alban Caré <alban.care@gmail.com>
2023-03-30 19:27:16 +0200

    reafct: add on EmailUpdateError action

--- src/middlewares/userMiddleware.js
+++ src/middlewares/userMiddleware.js
@@ -14,6 +14,7 @@ import {
   onSettingsUpdateError,
   ON_EMAIL_UPDATE,
   onEmailUpdateSuccess,
+  onEmailUpdateError,
   ON_EMAIL_VERIFICATION,
   onEmailVerificationSuccess,
   onEmailVerificationError,
@@ -134,6 +135,9 @@ const userMiddleware = (store) => (next) => (action) => {
           store.dispatch(
             onEmailUpdateSuccess(response.data.email, response.data.nickname),
           );
+        })
+        .catch((error) => {
+          store.dispatch(onEmailUpdateError(error));
         });
       break;
     case ON_EMAIL_VERIFICATION:
