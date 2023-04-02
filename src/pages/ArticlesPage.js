d4d78c1fe74d8221c8bdb3fb68f58d7ffdf43295
Alban Caré <alban.care@gmail.com>
2023-04-02 15:32:13 +0200

    feat: add search filter

--- src/pages/ArticlesPage.js
+++ src/pages/ArticlesPage.js
@@ -25,6 +25,7 @@ export default function ArticlesPage() {
   const [page, setPage] = useState(1);
   const [category, setCategory] = useState('All');
   const [sortBy, setSortBy] = useState('desc');
+  const [search, setSearch] = useState('');
   const [articles, setArticles] = useState([]);
   const categories = useSelector((state) => state.common.categories);
   const articlesList = useSelector((state) => state.articles.listPerPage);
@@ -48,6 +49,9 @@ export default function ArticlesPage() {
     if (sortBy !== 'desc') {
       params.push({ name: 'order', value: sortBy });
     }
+    if (search !== '') {
+      params.push({ name: 'search', value: search });
+    }
     dispatch(getArticles(params));
   }, [page, category]);
 
@@ -70,6 +74,21 @@ export default function ArticlesPage() {
     articles.reverse();
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPage(1);
+    if (search !== '') {
+      const params = [
+        { name: 'page', value: page },
+        { name: 'sorttype', value: 'updated_at' },
+      ];
+      if (category !== 'All') {
+        params.push({ name: 'category', value: category });
+      }
+      dispatch(getArticles(params));
+    }
+  };
+
   /* loader */
   if (articles.length === 0) {
     return (
@@ -86,77 +105,79 @@ export default function ArticlesPage() {
         subtitle="Découvrez tous nos articles"
         image="https://cdn.eco-friendly.fr/assets/img/misc/articles.webp"
       />
-      <Box
-        sx={{
-          marginY: 2,
-          marginX: 0,
-          display: 'flex',
-          flexDirection: { xs: 'column', sm: 'row' },
-        }}
-      >
-        <Box position="relative" sx={{ flexGrow: 1 }}>
-          <TextField
-            id="search-bar"
-            className="text"
-            onInput={(e) => {
-              console.log(e.target.value);
-            }}
-            label="Rechercher"
-            variant="outlined"
-            placeholder="Recherche..."
-            size="small"
-            fullWidth
-          />
-          <IconButton
-            type="submit"
-            aria-label="search"
-            sx={{
-              position: 'absolute',
-              right: 0,
-              top: 0,
-              bottom: 0,
-              m: 'auto',
-            }}
-          >
-            <SearchIcon />
-          </IconButton>
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <Box
+          sx={{
+            marginY: 2,
+            marginX: 0,
+            display: 'flex',
+            flexDirection: { xs: 'column', sm: 'row' },
+          }}
+        >
+          <Box position="relative" sx={{ flexGrow: 1 }}>
+            <TextField
+              id="search-bar"
+              className="text"
+              onInput={(e) => {
+                setSearch(e.target.value);
+              }}
+              label="Rechercher"
+              variant="outlined"
+              placeholder="Recherche..."
+              size="small"
+              fullWidth
+            />
+            <IconButton
+              type="submit"
+              aria-label="search"
+              sx={{
+                position: 'absolute',
+                right: 0,
+                top: 0,
+                bottom: 0,
+                m: 'auto',
+              }}
+            >
+              <SearchIcon />
+            </IconButton>
+          </Box>
+          <FormControl sx={{ mx: 2 }}>
+            <InputLabel id="category-label">Catégorie</InputLabel>
+            <Select
+              name="category"
+              labelId="category-label"
+              id="category"
+              label="Catégorie"
+              value={category}
+              onChange={changeFieldCategories}
+              size="small"
+            >
+              <MenuItem value="All">Toutes les catégories</MenuItem>
+              {categories.map((item) => (
+                <MenuItem key={item.id} value={item.id}>
+                  {item.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl>
+            <InputLabel id="category-label">Trier par</InputLabel>
+            <Select
+              name="sortby"
+              labelId="sortby-label"
+              id="sortby"
+              label="Trier par"
+              value={sortBy}
+              onChange={changeFieldSortBy}
+              size="small"
+            >
+              <MenuItem value="desc">le plus récent</MenuItem>
+              <MenuItem value="asc">le plus ancien</MenuItem>
+              {/* <MenuItem value="author">auteur</MenuItem> */}
+            </Select>
+          </FormControl>
         </Box>
-        <FormControl sx={{ mx: 2 }}>
-          <InputLabel id="category-label">Catégorie</InputLabel>
-          <Select
-            name="category"
-            labelId="category-label"
-            id="category"
-            label="Catégorie"
-            value={category}
-            onChange={changeFieldCategories}
-            size="small"
-          >
-            <MenuItem value="All">Toutes les catégories</MenuItem>
-            {categories.map((item) => (
-              <MenuItem key={item.id} value={item.id}>
-                {item.name}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <FormControl>
-          <InputLabel id="category-label">Trier par</InputLabel>
-          <Select
-            name="sortby"
-            labelId="sortby-label"
-            id="sortby"
-            label="Trier par"
-            value={sortBy}
-            onChange={changeFieldSortBy}
-            size="small"
-          >
-            <MenuItem value="desc">le plus récent</MenuItem>
-            <MenuItem value="asc">le plus ancien</MenuItem>
-            {/* <MenuItem value="author">auteur</MenuItem> */}
-          </Select>
-        </FormControl>
-      </Box>
+      </form>
       <Grid container spacing={2}>
         {articles.map((article) => (
           <Grid item xs={12} sm={6} md={4} lg={3} key={`${article.id}`}>
